27187f1722c71132f1a28f1fe51c2a530a7880bd
BT <bitaniagezahegn3@gmail.com>
2026-02-17 02:53:03 +0300

    added a PWA manifest file so this can be installed as an app on mobile

--- script.js
+++ script.js
@@ -940,3 +940,10 @@ async function predictWebcam() {
 
     window.requestAnimationFrame(predictWebcam);
 }
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js');
+    });
+}
